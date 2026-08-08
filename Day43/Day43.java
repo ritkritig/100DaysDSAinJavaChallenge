d7f5b8e852c65e5ceaac18c794844940fec7b472
ritkritig <ritkritigoyal@gmail.com>
2026-08-08 20:51:10 +0530

    day43: Binary Tree Construction (Level Order)

---
+++ Day43/Day43.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+public class Main {
+
+    static Node buildTree(int[] arr) {
+        if (arr.length == 0 || arr[0] == -1) {
+            return null;
+        }
+
+        Node root = new Node(arr[0]);
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        int i = 1;
+
+        while (i < arr.length) {
+            Node current = queue.poll();
+
+            // Left child
+            if (i < arr.length && arr[i] != -1) {
+                current.left = new Node(arr[i]);
+                queue.add(current.left);
+            }
+            i++;
+
+            // Right child
+            if (i < arr.length && arr[i] != -1) {
+                current.right = new Node(arr[i]);
+                queue.add(current.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Node root = buildTree(arr);
+
+        inorder(root);
+
+        sc.close();
+    }
+}
