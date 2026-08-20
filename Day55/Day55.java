e25a00db54a092077ba6a0cfea009ae11a369946
ritkritig <ritkritigoyal@gmail.com>
2026-08-20 21:04:51 +0530

    Day55

---
+++ Day55/Day55.java
@@ -0,0 +1,95 @@
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class Day55 {
+
+    static Node buildTree(int[] arr) {
+        if (arr.length == 0 || arr[0] == -1) {
+            return null;
+        }
+
+        Node root = new Node(arr[0]);
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        int i = 1;
+
+        while (!queue.isEmpty() && i < arr.length) {
+            Node current = queue.poll();
+
+            if (i < arr.length && arr[i] != -1) {
+                current.left = new Node(arr[i]);
+                queue.add(current.left);
+            }
+            i++;
+
+            if (i < arr.length && arr[i] != -1) {
+                current.right = new Node(arr[i]);
+                queue.add(current.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static void rightView(Node root) {
+
+        if (root == null) {
+            return;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+
+                Node current = queue.poll();
+
+                // Last node of every level
+                if (i == size - 1) {
+                    System.out.print(current.data + " ");
+                }
+
+                if (current.left != null) {
+                    queue.add(current.left);
+                }
+
+                if (current.right != null) {
+                    queue.add(current.right);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Node root = buildTree(arr);
+
+        rightView(root);
+
+        sc.close();
+    }
+}
