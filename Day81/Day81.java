d4f9761ea12cc35f0aed8ee29ba059f28d4f58de
ritkritig <ritkritigoyal@gmail.com>
2026-09-15 21:49:43 +0530

    Day81

---
+++ Day81/Day81.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Day81 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Bubble Sort
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        // Print sorted array
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]);
+
+            if (i < n - 1) {
+                System.out.print(" ");
+            }
+        }
+    }
+}
