ac0bd7ae6261bca13b2215c2904451d7190aa414
ritkritig <ritkritigoyal@gmail.com>
2026-09-16 20:38:06 +0530

    Day82

---
+++ Day82/Day82.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+class Day82 {
+
+    public static int lowerBound(int[] arr, int x) {
+        int low = 0;
+        int high = arr.length - 1;
+        int ans = arr.length;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] >= x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static int upperBound(int[] arr, int x) {
+        int low = 0;
+        int high = arr.length - 1;
+        int ans = arr.length;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] > x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int x = sc.nextInt();
+
+        int lb = lowerBound(arr, x);
+        int ub = upperBound(arr, x);
+
+        System.out.println(lb + " " + ub);
+    }
+}
